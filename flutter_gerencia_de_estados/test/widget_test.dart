4efd8746bf4aff8efd947e79c60e514400ff9d4c
Orlando Eduardo Pereira <orlando.eduardo101@gmail.com>
2021-01-13 23:30:39 -0300

    fix

--- flutter_gerencia_de_estados/test/widget_test.dart
+++ flutter_gerencia_de_estados/test/widget_test.dart
@@ -6,9 +6,9 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/my_app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_redux/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
